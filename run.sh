bf0e6c98894a3030a5d50e4960becf860e82fc4b
ingmapping <31092728+ingmapping@users.noreply.github.com>
2018-07-09 09:50:10 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -17,6 +17,9 @@ cd ~/Documents/MapBox/project/
 wget https://ingmapping.com/openstreets-nl/openstreets-nl.zip
 unzip openstreets-nl 
 
+wget https://ingmapping.com/openstreets-nl/openstreets-nl-rd.zip
+unzip openstreets-nl 
+
 docker run -d --name="docker-tilemill" -p 20008:20008 -p 20009:20009 -v ~/Documents/MapBox/project/:/root/Documents/MapBox/project/ -t ingmapping/tilemill
 
 
